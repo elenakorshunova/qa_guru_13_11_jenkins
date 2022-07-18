b7602ddfe24d3f84aeaff3e9f9b6c5bc5b4398d6
Elena Korshunova <ev.korshunowa@gmail.com>
2022-07-18 09:57:14 +0300

    homework

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,6 +14,7 @@ import pages.RegistrationFormPage;
 public class TestBase {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    public static CredentialsConfig credentialsConfig = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     static void setUp() {
@@ -23,8 +26,14 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        String remoteBrowser = System.getProperty("remote", "selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = String.format("https://%s:%s@%s", credentialsConfig.login(), credentialsConfig.password(),
+                remoteBrowser);
+
     }
 
     @AfterEach
